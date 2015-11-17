921ae8ff519e3f8c3c0b857d9137c942992e5d61
James Bouwens <potatoesroll@gmail.com>
2015-11-17 15:50:50 -0500

    Missing files. FileExtensions.cs and UriMapper.cs added.

---
+++ Put.io.Core/Extensions/FileExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Put.io.Core.Extensions
+{
+	class FileExtensions
+	{
+		public static async Task<FileStream> ReadFromFile(string fileName)
+		{
+            var file = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+			return (await file.OpenReadAsync()).AsStreamForRead() as FileStream;
+		}
+	}
+}
---
+++ Put.io.Wp/Helpers/UriMapper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Navigation;
+using Windows.Phone.Storage.SharedAccess;
+
+namespace Helpers
+{
+
+
+public class UriMapper : UriMapperBase
+{
+	private string tempUri;
+
+	public override Uri MapUri(Uri uri)
+	{
+		tempUri = uri.ToString();
+
+		// File association launch
+		if (tempUri.Contains("/FileTypeAssociation"))
+		{
+			// Get the file ID (after "fileToken=").
+			int fileIDIndex = tempUri.IndexOf("fileToken=") + 10;
+			string fileID = tempUri.Substring(fileIDIndex);
+
+			// Get the file name.
+			string incomingFileName =
+				SharedStorageAccessManager.GetSharedFileName(fileID);
+
+			// Get the file extension.
+			int extensionIndex = incomingFileName.LastIndexOf('.') + 1;
+			string incomingFileType =
+				incomingFileName.Substring(extensionIndex).ToLower();
+
+			// Map the .log files to the appropriate pages.
+			switch (incomingFileType)
+			{
+				case "torrent":
+					return new Uri("/Views/MainPage.xaml?fileToken=" + fileID, UriKind.Relative);
+				default:
+					return new Uri("/App.xaml", UriKind.Relative);
+			}
+
+		}
+        if (tempUri.Contains("magnet:"))
+        {
+            // Otherwise perform normal launch.
+            return uri;
+        }
+            // Otherwise perform normal launch.
+            return uri;
+	}
+}
+}
